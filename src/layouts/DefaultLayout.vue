9e76bbb005cf48c995c8881d27e952dfd6f1f6af
ishmamazim <ishmamazim2@gmail.com>
2024-01-11 21:48:51 +0600

    vue-3 boilerplate

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,24 @@
+<template>
+   <div>
+    <header>
+     <Navbar />
+    </header>
+    <main>
+    
+      <slot /> <!-- This is where the page content will be rendered -->
+    </main>
+    <footer>
+      <!-- Footer content -->
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Navbar from './Navbar.vue';
+
+
+</script>
+
+<style>
+/* Add your CSS styles for the layout here */
+</style>
