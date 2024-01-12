67f180922132d3cf8f87012b120fbec7b84b6f27
ishmamazim <ishmamazim2@gmail.com>
2024-01-13 01:16:37 +0600

    card

---
+++ src/components/cardSection.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="bg-white rounded-lg">   
+        <h1 class="text-slate-800 text-4xl">Nikah Policy and Procedure</h1>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
