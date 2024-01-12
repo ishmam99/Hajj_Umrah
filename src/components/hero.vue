a2205f36778c2ef948266d66259584bad689c044
Arif Akib <a.rabbani.akib@gmail.com>
2024-01-13 01:19:28 +0600

    navbar-hero-foot

---
+++ src/components/hero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="h-[100svh] overflow-hidden w-full relative border px-[5%]">
+        <img src="../assets/image/hero/h1.png" alt="" class="absolute top-0 right-0 h-full w-full z-[-10]">
+        <div class="flex flex-col items-left justify-center h-full w-3/5 gap-3">
+            <h2 class="text-5xl font-bold text-left text-gray-600">Welcome to  </h2>
+            <h1 class="text-6xl font-bold text-left">ISLAMIC CENTER</h1>
+            <button class="h-[45px] w-[130px] text-yellow-600 font-semibold border-2 border-yellow-600 rounded-lg uppercase text-sm">Join us</button>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
+
+<style>
+
+</style>
