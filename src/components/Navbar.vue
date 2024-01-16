52b96d5ea73464fae2d9c01e71ab0297abc33569
Arif Akib <a.rabbani.akib@gmail.com>
2024-01-16 23:21:58 +0600

    NavbarEdit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="h-[70px] w-full flex justify-between items-center text-black fixed top-0 z-40 px-[5%] bg-white duration-300 " :class="searchBar==true ? '':'bg-slate-50'" id="navBar">  
-      <div>
-        <img src="/src/assets/image/common/n1.png" alt="" class="h-[50px]">
-      </div> 
-      <div class="flex gap-6 uppercase font-semibold">
+      
+      <div class="flex gap-6 uppercase text-sm font-semibold">
+          <div>
+            <img src="/src/assets/image/common/n1.png" alt="" class="h-[50px]">
+          </div> 
           <router-link to="/" class="py-6 flex items-center">
               Home
           </router-link>  
@@ -59,16 +60,20 @@
             <router-link to="/Blog" class="py-6 flex items-center">Event</router-link>
           </ul>
           <ul class="group cursor-pointer relative ">
+            <router-link to="/Blog" class="py-6 flex items-center">Volunteer</router-link>
+          </ul>
+          <ul class="group cursor-pointer relative ">
             <router-link to="/Blog" class="py-6 flex items-center">More</router-link>
             <ul class="w-0 group-hover:w-[450px] group-hover:h-auto h-0 p-0 group-hover:p-5 overflow-hidden flex flex-col gap-2 bg-slate-50 absolute top-[101%] rounded-md duration-200">
                 <router-link to="/Shop" class="hover:pl-5 duration-300">Future Projects</router-link>
                 <!-- <li class="hover:pl-5 duration-300">Dua</li> -->
                 <li class="hover:pl-5 duration-300">Ramadan Special</li>
                 <li class="hover:pl-5 duration-300">Hajj And Umrah</li>
+                <li class="hover:pl-5 duration-300">Registration</li>
             </ul>
           </ul>
       </div>
-      <div class="flex items-center gap-2 text-white font-bold">
+      <div class="flex items-center gap-2 text-white font-bold text-sm">
         <button class="bg-yellow-600 px-6 py-1 rounded-full hover:bg-yellow-500">Donation</button>
         <button class="bg-yellow-600 px-6 py-1 rounded-full hover:bg-yellow-500">Member</button>
       </div>
