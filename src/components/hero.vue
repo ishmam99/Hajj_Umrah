f7e7b5e785835ca09302e48156348764cab69152
Arif Akib <a.rabbani.akib@gmail.com>
2024-01-25 20:33:13 +0600

    join

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -4,7 +4,7 @@
         <div class="flex flex-col items-left justify-center h-full w-3/5 gap-3">
             <h2 class="text-5xl font-bold text-left text-gray-600">Welcome to  </h2>
             <h1 class="text-6xl font-bold text-left">ISLAMIC CENTER</h1>
-            <button class="h-[45px] w-[130px] text-yellow-600 font-semibold border-2 border-yellow-600 rounded-lg uppercase text-sm">Join us</button>
+            <router-link to="/Signup" class="h-[45px] w-[130px] flex items-center justify-center text-yellow-600 font-semibold border-2 border-yellow-600 rounded-lg uppercase text-sm">Join us</router-link>
         </div>
     </div>
 </template>
