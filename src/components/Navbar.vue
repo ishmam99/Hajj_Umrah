75d51ef70f015510edfd3c4e82650ac132f3a4cf
Arif Akib <a.rabbani.akib@gmail.com>
2024-01-27 02:23:40 +0600

    edit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -282,13 +282,11 @@
             <MenubarTrigger>Member<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="rgba(0,0,0,1)"><path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path></svg></MenubarTrigger>
             <MenubarContent>
               <MenubarItem>
-<<<<<<< HEAD
+
                 <router-link to="/Login" class=" w-full hover:pl-5 duration-300">Login</router-link>
               </MenubarItem>
               <MenubarItem>
                 <router-link to="/more/registration/memebship_registration" class=" w-full hover:pl-5 duration-300">Sign up</router-link>
-=======
->>>>>>> 5db6dd0a7843cf7220ca1300f7c7cfa9071eab7e
               </MenubarItem>
 
             </MenubarContent>
