a984ed70b6394bf384f91a49ec2a32ba83e934ab
ishmamazim <ishmamazim2@gmail.com>
2024-02-01 21:26:41 +0600

    api

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,10 +1,9 @@
 import axios from 'axios'
-const api = axios.create({
-  baseURL: 'https://masjid.islamicdigitallane.com/api/v1',
-  headers: {
-    'Content-Type': 'application/json',
-    Accept: 'application/json'
-  }
-})
-
-export default api
+export default () =>
+  axios.create({
+    baseURL: 'https://masjid.islamicdigitallane.com/api/v1',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    }
+  })
