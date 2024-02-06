9d65595262d9ea7a4c3a9804b572a9022ec62303
Rasik Abdullah <rasikabdullah@gmail.com>
2024-02-07 03:31:19 +0600

    update

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,34 +1,34 @@
 <template>
-   <div>
+  <div>
     <header>
-     <Navbar/>
+      <Navbar />
     </header>
-    <main class="h-full"> 
-      <div >
-           <slot /> <!-- This is where the page content will be rendered -->
+    <main class="h-full">
+      <div>
+        <Toaster />
+        <slot />
+        <!-- This is where the page content will be rendered -->
       </div>
-   
     </main>
-    
-      
-      <foot/>
+
+    <foot />
   </div>
 </template>
 
 <script setup lang="ts">
-import Navbar from '../components/Navbar.vue';
+import Navbar from '../components/Navbar.vue'
 import foot from '../components/footer.vue'
-
+import Toaster from '/components/ui/toast/Toaster.vue'
 import { ref, onMounted } from 'vue'
-import {useStore} from '../stores/store.ts'
-import {useAuthStore} from '../stores/AuthStore.ts'
+import { useStore } from '../stores/store.ts'
+import { useAuthStore } from '../stores/AuthStore.ts'
 const store = useStore()
 const authStore = useAuthStore()
 
 const gatprayerTime = async () => {
   const { data } = await api().get('get-prayer-time')
-    console.log(data)
-    store.prayertime = data
+  console.log(data)
+  store.prayertime = data
 }
 // const getCategories =async() =>{
 //     const { data } = await api().get('product-category')
@@ -46,28 +46,26 @@ const getServiceList = async () => {
   store.serviceList = data
   // console.log(store.serviceList[0])
 }
-const getEvent=async()=>{
-    const { data } = await api().get('get-event')
-    // console.log(data)
-    store.event = data
+const getEvent = async () => {
+  const { data } = await api().get('get-event')
+  // console.log(data)
+  store.event = data
 }
-const getAnnouncement=async()=>{
-    const { data } = await api().get('get-announcement')
-    // console.log(data)
-    store.announcement = data
+const getAnnouncement = async () => {
+  const { data } = await api().get('get-announcement')
+  // console.log(data)
+  store.announcement = data
 }
-const getDonationType=async()=>{
-    const { data } = await api().get('all-donation-types')
-    // console.log(data)
-    store.donationType = data
+const getDonationType = async () => {
+  const { data } = await api().get('all-donation-types')
+  // console.log(data)
+  store.donationType = data
 }
-onMounted(async() => {
+onMounted(async () => {
   gatprayerTime()
   getEvent()
   getAnnouncement()
   getServiceList()
   getDonationType()
 })
-
 </script>
-
--- src/views/More/RamadanSpecial.vue
+++ src/views/More/RamadanSpecial.vue
@@ -41,7 +41,6 @@
       >
         <div class="space-y-8 w-3/4">
           <form @submit.prevent="ramadanProgramSubmission()">
-
             <div class="bg-white rounded-xl p-5 w-full">
               <p class="pt-5 text-lg font-semibold">Choose the program/s that you interested in:</p>
               <div class="pt-5 ml-10">
@@ -53,7 +52,6 @@
                       name="Event_Time"
                       value="Khatma"
                       class="h-3 w-3 px-2"
-                      v-model="ramadanProgram.membership_type"
                     />
                     <label for="Asr" class="text-lg">RAMADAN KHATMA</label>
                   </div>
@@ -64,7 +62,6 @@
                       name="Event_Time"
                       value="Kahf"
                       class="h-3 w-3 px-2"
-                      v-model="ramadanProgram.membership_type"
                     />
                     <label for="Asr" class="text-lg">Surat Al-Kahf</label>
                   </div>
@@ -75,7 +72,6 @@
                       name="Event_Time"
                       value="Hafiz"
                       class="h-3 w-3 px-2"
-                      v-model="ramadanProgram.membership_type"
                     />
                     <label for="Asr" class="text-lg">Imam and Hafiz Program</label>
                   </div>
@@ -86,7 +82,6 @@
                       name="Event_Time"
                       value="Stories"
                       class="h-3 w-3 px-2"
-                      v-model="ramadanProgram.membership_type"
                     />
                     <label for="Asr" class="text-lg">Best Stories</label>
                   </div>
@@ -97,7 +92,6 @@
                       name="Event_Time"
                       value="Fiqh"
                       class="h-3 w-3 px-2"
-                      v-model="ramadanProgram.membership_type"
                     />
                     <label for="Asr" class="text-lg">Fiqh of Fasting</label>
                   </div>
@@ -108,25 +102,38 @@
                       name="Event_Time"
                       value="Azkar"
                       class="h-3 w-3 px-2"
-                      v-model="ramadanProgram.membership_type"
                     />
                     <label for="Asr" class="text-lg">Azkar Program</label>
                   </div>
                 </div>
               </div>
             </div>
-  
+
             <div class="bg-white rounded-xl p-5 w-full">
               <p class="text-xl font-bold pt-2 pb-2">
                 Membership Type<span class="text-red-600">*</span>
               </p>
               <div class="grid grid-cols-1 gap-2">
                 <div class="flex items-center gap-2 font-semibold">
-                  <input id="Asr" type="radio" name="Event_Time" value="Dhur" class="h-3 w-3" />
+                  <input
+                    id="Asr"
+                    type="radio"
+                    name="Event_Time"
+                    value="Individual"
+                    class="h-3 w-3"
+                    v-model="ramadanProgram.membership_type"
+                  />
                   <label for="Dhur">Individual (Masjid Membership)</label>
                 </div>
                 <div class="flex items-center gap-2 font-semibold">
-                  <input type="radio" id="Asr" name="Event_Time" value="Asr" class="h-3 w-3" />
+                  <input
+                    type="radio"
+                    id="Asr"
+                    name="Event_Time"
+                    value="Family"
+                    class="h-3 w-3"
+                    v-model="ramadanProgram.membership_type"
+                  />
                   <label for="Asr">Family (Masjid Membership)</label>
                 </div>
               </div>
@@ -141,6 +148,7 @@
                       class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
                       id="exampleFormControlInput50"
                       value=""
+                      v-model="ramadanProgram.first_name"
                     />
                     <label
                       for="exampleFormControlInput50"
@@ -154,6 +162,7 @@
                       class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
                       id="exampleFormControlInput50"
                       value=""
+                      v-model="ramadanProgram.last_name"
                     />
                     <label
                       for="exampleFormControlInput50"
@@ -169,6 +178,7 @@
                       class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
                       id="exampleFormControlInput50"
                       value=""
+                      v-model="ramadanProgram.email"
                     />
                     <label
                       for="exampleFormControlInput50"
@@ -182,6 +192,7 @@
                       class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
                       id="exampleFormControlInput50"
                       value=""
+                      v-model="ramadanProgram.phone_number"
                     />
                     <label
                       for="exampleFormControlInput50"
@@ -197,6 +208,7 @@
                       class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
                       id="exampleFormControlInput50"
                       value=""
+                      v-model="ramadanProgram.address"
                     />
                     <label
                       for="exampleFormControlInput50"
@@ -211,6 +223,7 @@
                         class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
                         id="exampleFormControlInput50"
                         value=""
+                        v-model="ramadanProgram.city"
                       />
                       <label
                         for="exampleFormControlInput50"
@@ -224,6 +237,7 @@
                         class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
                         id="exampleFormControlInput50"
                         value=""
+                        v-model="ramadanProgram.state"
                       />
                       <label
                         for="exampleFormControlInput50"
@@ -240,6 +254,7 @@
                       <input
                         type="date"
                         class="py-2 px-6 rounded-2xl w-full border-2 border-grau-400"
+                        v-model="ramadanProgram.dob"
                       />
                     </div>
                   </div>
@@ -253,7 +268,13 @@
                   Operations
                 </p>
                 <div class="flex items-center gap-2 py-2 px-2">
-                  <input type="checkbox" name="" id="" class="h-3 w-3" />
+                  <input
+                    type="checkbox"
+                    name=""
+                    id=""
+                    class="h-3 w-3"
+                    v-model="ramadanProgram.donation"
+                  />
                   <label for="" class="text-xs"
                     >Donated ($100 for family or $50 for individual or more)</label
                   >
@@ -266,11 +287,25 @@
               </p>
               <div class="grid grid-cols-2 gap-2 px-2">
                 <div class="flex items-center gap-2">
-                  <input id="Asr" type="checkbox" name="Event_Time" value="Dhur" class="h-3 w-3" />
+                  <input
+                    id="Asr"
+                    type="radio"
+                    name="policy"
+                    value="Individual"
+                    class="h-3 w-3"
+                    v-model="ramadanProgram.policy_type"
+                  />
                   <label for="Dhur" class="text-xs">Individual (Masjid Membership)</label>
                 </div>
                 <div class="flex items-center gap-2">
-                  <input type="checkbox" id="Asr" name="Event_Time" value="Asr" class="h-3 w-3" />
+                  <input
+                    type="radio"
+                    id="Asr"
+                    name="policy"
+                    value="Family"
+                    class="h-3 w-3"
+                    v-model="ramadanProgram.policy_type"
+                  />
                   <label for="Asr" class="text-xs">Family (Masjid Membership)</label>
                 </div>
               </div>
@@ -283,6 +318,7 @@
                     type="text"
                     placeholder="100 USD"
                     class="py-2 px-4 rounded-2xl w-50 shadow-lg border border-black"
+                    v-model="ramadanProgram.due"
                   />
                 </div>
               </div>
@@ -295,11 +331,13 @@
                     type="text"
                     placeholder="First Name"
                     class="py-2 px-4 rounded-2xl w-1/2 shadow-lg border border-black"
+                    v-model="ramadanProgram.card_first_name"
                   />
                   <input
                     type="text"
                     placeholder="Last Name"
                     class="py-2 px-4 rounded-2xl w-1/2 shadow-lg border border-black"
+                    v-model="ramadanProgram.card_last_name"
                   />
                 </div>
                 <div class="flex gap-10">
@@ -310,6 +348,7 @@
                       type="text"
                       placeholder="1111 2222 3333 4444 5555"
                       class="py-2 w-1/2 rounded-2xl"
+                      v-model="ramadanProgram.card_number"
                     />
                     <img src="/src/assets/image/common/ft5.png" alt="" class="h-5" />
                   </div>
@@ -317,41 +356,82 @@
                     <input
                       type="date"
                       class="py-2 px-4 rounded-2xl w-1/2 shadow-lg border border-black"
+                      v-model="ramadanProgram.card_date"
                     />
                     <input
                       type="text"
                       placeholder="CVC"
                       class="py-2 px-4 rounded-2xl w-1/2 shadow-lg border border-black"
+                      v-model="ramadanProgram.cvc"
                     />
                   </div>
                 </div>
-                <Select class="">
+                <Select class="" v-model="ramadanProgram.country">
                   <SelectTrigger class="w-full rounded-2xl py-2 px-4 border border-black">
                     <SelectValue placeholder="Country" />
                   </SelectTrigger>
                   <SelectContent>
                     <SelectGroup>
                       <SelectLabel>Country</SelectLabel>
-                      <SelectItem value="apple"> BanglaDesh </SelectItem>
-                      <SelectItem value="banana"> UsA </SelectItem>
-                      <SelectItem value="blueberry"> Canada </SelectItem>
-                      <SelectItem value="grapes"> Australia </SelectItem>
-                      <SelectItem value="pineapple"> Rasia </SelectItem>
+                      <SelectItem value="Bangladesh"> Bangladesh </SelectItem>
+                      <SelectItem value="USA"> USA </SelectItem>
+                      <SelectItem value="Canada"> Canada </SelectItem>
+                      <SelectItem value="Australia"> Australia </SelectItem>
+                      <SelectItem value="UK"> UK </SelectItem>
+                      <SelectItem value="Turkey"> Turkey </SelectItem>
+                      <SelectItem value="Russia"> Russia </SelectItem>
+                      <SelectItem value="Pakistan"> Pakistan </SelectItem>
+                      <SelectItem value="Syria"> Syria </SelectItem>
+                      <SelectItem value="Sudan"> Sudan </SelectItem>
+                      <SelectItem value="Morocco"> Morocco </SelectItem>
                     </SelectGroup>
                   </SelectContent>
                 </Select>
+
                 <button
-                  type="submit"
+                  :disabled="loading"
                   class="w-[350px] h-[45px] rounded-2xl bg-emerald-800 text-white font-bold mx-auto"
+                  type="submit"
                 >
-                  Apply
+                  <p v-if="loading" class="flex items-center justify-center">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="#fff"
+                      class="animate-spin w-5 h-5"
+                    >
+                      <path
+                        d="M18.364 5.63604L16.9497 7.05025C15.683 5.7835 13.933 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12H21C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C14.4853 3 16.7353 4.00736 18.364 5.63604Z"
+                      ></path>
+                    </svg>
+                    Creating...
+                  </p>
+                  <p v-else>Apply</p>
                 </button>
-  
+
+                <!-- <button
+                  :disabled="loading"
+                  class="text-white font-semibold text-sm px-3 py-3 rounded-lg bg-green-600 mt-2"
+                  type="submit"
+                >
+                  <p v-if="loading" class="flex">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 24 24"
+                      fill="#fff"
+                      class="animate-spin w-5 h-5"
+                    >
+                      <path
+                        d="M18.364 5.63604L16.9497 7.05025C15.683 5.7835 13.933 5 12 5C8.13401 5 5 8.13401 5 12C5 15.866 8.13401 19 12 19C15.866 19 19 15.866 19 12H21C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C14.4853 3 16.7353 4.00736 18.364 5.63604Z"
+                      ></path>
+                    </svg>
+                    Creating...
+                  </p>
+                  <p v-else>Create Your Fundraising</p>
+                </button> -->
               </div>
             </div>
-
           </form>
-
         </div>
       </div>
     </DefaultLayout>
@@ -363,6 +443,7 @@ import DefaultLayout from '/src/layouts/DefaultLayout.vue'
 import Card from '/src/components/cardSection.vue'
 import RegistrationMore from '/src/components/RgistrationMore.vue'
 import { ref } from 'vue'
+import { useToast } from '/components/ui/toast/use-toast'
 
 import {
   Select,
@@ -373,6 +454,9 @@ import {
   SelectTrigger,
   SelectValue
 } from '/components/ui/select'
+
+const loading = ref(false)
+const { toast } = useToast()
 
 const ramadanProgram = ref({
   membership_type: {},
@@ -389,16 +473,33 @@ const ramadanProgram = ref({
   policy_type: '',
   card_first_name: '',
   card_last_name: '',
+  card_number: '',
   card_date: '',
   cvc: '',
   country: ''
 })
 
-const ramadanProgramSubmission = () => {
-
+const ramadanProgramSubmission = async () => {
+  loading.value = true
+  try {
+    const data = await api().post('ramadan-program-request', {
+      method: 'post',
+      body: ramadanProgram
+    })
+    toast({
+      title: 'Success',
+      description: data.data.message
+    })
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+    toast({
+      title: 'Error',
+      description: 'Please Try Again'
+    })
+  }
+  loading.value = false
 }
-
-
 </script>
 
 <style lang="scss" scoped></style>
