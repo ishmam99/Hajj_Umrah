7ba9965c74c9497d83c00458311c411052553f58
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-02-10 03:20:16 +0600

    table

--- src/views/Hr/EmploymentScheduling.vue
+++ src/views/Hr/EmploymentScheduling.vue
@@ -4,99 +4,90 @@
       <div class="pt-[80px] flex justify-between">
         <hrSidebar />
         <div class="px-5 bg-slate-50 py-5 w-3/4">
-          <p class="text-2xl font-bold py-3 border-b">Employment Scheduling</p>
+          <p class="text-2xl font-bold py-3 border-2-b">Employment Scheduling</p>
           <div class="bg-white rounded-xl p-5 w-full shadow-md mt-5">
 
             <!-- ekhan theke fele diben -->
-
-            <form @submit.prevent="volunteerAdmissionSubmit()">
-              <div class="py-5 space-y-7 mt-5">
-                <div class="flex gap-4">
-                  <div class="relative mb-3 w-1/2">
-                    <input
-                      type="text"
-                      class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
-                      id="exampleFormControlInput50"
-                      value=""
-                    />
-                    <label
-                      for="exampleFormControlInput50"
-                      class="absolute left-3 top-0 mb-0 font-bold z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
-                      >Order Name <span class="text-red-500">*</span>
-                    </label>
-                  </div>
-                  <div class="relative mb-3 w-1/2">
-                    <input
-                      type="number"
-                      class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
-                      id="exampleFormControlInput50"
-                      value=""
-                    />
-                    <label
-                      for="exampleFormControlInput50"
-                      class="absolute left-3 font-semibold top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
-                      >Budget <span class="text-red-500">*</span>
-                    </label>
-                  </div>
-                </div>
-                <div class="relative">
-                  <p class="p-3 absolute top-[-25px] left-2 bg-white text-gray-600">
-                    Order Description
-                  </p>
-                  <textarea
-                    name=""
-                    id=""
-                    cols=""
-                    rows="4"
-                    placeholder="Ex:23"
-                    class="w-full p-3 pt-5 rounded-lg border-2 focus:outline-gray-200"
-                  ></textarea>
-                </div>
-                <div class="relative">
-                  <p class="p-3 absolute top-[-25px] left-2 bg-white text-gray-600">Item List</p>
-                  <textarea
-                    name=""
-                    id=""
-                    cols=""
-                    rows="4"
-                    placeholder="Ex:23"
-                    class="w-full p-3 pt-5 rounded-lg border-2 focus:outline-gray-200"
-                  ></textarea>
-                </div>
-
-                <div class="flex gap-5">
-                  <div class="relative w-1/2">
-                    <div class="gap-5">
-                      <label for="">Delivery Date <span class="text-red-500">*</span> </label>
-                      <div class="pt-2">
-                        <input
-                          type="date"
-                          class="py-2 px-6 rounded-2xl w-full border-2 border-grau-400"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div class="relative w-1/2">
-                    <div class="gap-5">
-                      <label for="">Delivery Time <span class="text-red-500">*</span> </label>
-                      <div class="pt-2">
-                        <input
-                          type="time"
-                          class="py-2 px-6 rounded-2xl w-full border-2 border-grau-400"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <button
-                  type="submit"
-                  class="w-full h-[45px] rounded-2xl bg-teal-800 text-white font-bold mx-auto flex items-center text-center justify-center"
-                >
-                  Create
-                </button>
-              </div>
-            </form>
-
+            <div class="overflow-x-auto">
+  <table class="table-auto border-2-collapse w-full">
+   
+    <tbody>
+      <tr>
+        <td class="border-2 px-4 py-2">Feb 2024, 2/12/2024 - 2/18/2024</td>
+      </tr>
+       <thead>
+      <tr>
+        <th class="border-2 px-4 py-2">Date</th>
+        <th class="border-2 px-4 py-2">Day</th>
+        <th class="border-2 px-4 py-2">From</th>
+        <th class="border-2 px-4 py-2">To</th>
+        <th class="border-2 px-4 py-2">From</th>
+        <th class="border-2 px-4 py-2">To</th>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">2/12/2024</td>
+        <td class="border-2 px-4 py-2">Mon</td>
+        <td class="border-2 px-4 py-2" colspan="2">Karim</td>
+        <td class="border-2 px-4 py-2" colspan="2">Jashim</td>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">2/13/2024</td>
+        <td class="border-2 px-4 py-2">Thu</td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">2/14/2024</td>
+        <td class="border-2 px-4 py-2">Wed</td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">2/15/2024</td>
+        <td class="border-2 px-4 py-2">Thu</td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">2/16/2024</td>
+        <td class="border-2 px-4 py-2">Fri</td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">2/7/2024</td>
+        <td class="border-2 px-4 py-2">Sat</td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">2/8/2024</td>
+        <td class="border-2 px-4 py-2">Sun</td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2"></td>
+      </tr>
+      <tr>
+        <td class="border-2 px-4 py-2">Total Hours</td>
+        <td class="border-2 px-4 py-2"></td>
+        <td class="border-2 px-4 py-2" colspan="2">40 hours</td>
+        <td class="border-2 px-4 py-2" colspan="2">40 hours</td>
+      </tr>
+    </thead>
+    </tbody>
+  </table>
+</div>
             <!-- ekhan porjonto -->
 
 
