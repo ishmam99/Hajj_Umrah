ab8c2b32f8ecbda330980f0665c7656e0e49c8d1
Rasik Abdullah <rasikabdullah@gmail.com>
2024-02-10 00:43:35 +0600

    update

---
+++ src/views/Supply/CreateSupplier.vue
@@ -0,0 +1,181 @@
+<template>
+  <DefaultLayout>
+    <div class="bg-slate-50">
+      <div class="pt-[80px] flex justify-between">
+        <SupplySidebar />
+        <div class="px-5 bg-slate-50 py-5 w-3/4">
+          <p class="text-2xl font-bold py-3 border-b">Supplier Create Form</p>
+          <div class="bg-white rounded-xl p-5 w-full shadow-md mt-5">
+            <form @submit.prevent="volunteerAdmissionSubmit()">
+              <div class="py-5 space-y-7 mt-5">
+                <div class="flex w-full gap-5">
+                  <div class="relative mb-3 w-full">
+                    <input
+                      type="text"
+                      class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
+                      id="exampleFormControlInput50"
+                      value=""
+                    />
+                    <label
+                      for="exampleFormControlInput50"
+                      class="absolute left-3 top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
+                      >First Name <span class="text-red-500">*</span>
+                    </label>
+                  </div>
+                  <div class="relative mb-3 w-full">
+                    <input
+                      type="text"
+                      class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
+                      id="exampleFormControlInput50"
+                      value=""
+                    />
+                    <label
+                      for="exampleFormControlInput50"
+                      class="absolute left-3 top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
+                      >Last Name <span class="text-red-500">*</span>
+                    </label>
+                  </div>
+                </div>
+
+                <div class="flex w-full gap-5">
+                  <div class="relative mb-3 w-full">
+                    <input
+                      type="text"
+                      class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
+                      id="exampleFormControlInput50"
+                      value=""
+                    />
+                    <label
+                      for="exampleFormControlInput50"
+                      class="absolute left-3 top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
+                      >Email <span class="text-red-500">*</span>
+                    </label>
+                  </div>
+                  <div class="relative mb-3 w-full">
+                    <input
+                      type="text"
+                      class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
+                      id="exampleFormControlInput50"
+                      value=""
+                    />
+                    <label
+                      for="exampleFormControlInput50"
+                      class="absolute left-3 top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
+                      >Phone Number <span class="text-red-500">*</span>
+                    </label>
+                  </div>
+                </div>
+
+                <div class="flex w-full gap-5">
+                  <div class="relative mb-3 w-full">
+                    <input
+                      type="text"
+                      class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
+                      id="exampleFormControlInput50"
+                      value=""
+                    />
+                    <label
+                      for="exampleFormControlInput50"
+                      class="absolute left-3 top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
+                      >Address <span class="text-red-500">*</span>
+                    </label>
+                  </div>
+                  <div class="flex w-full gap-5">
+                    <div class="relative mb-3 w-full">
+                      <input
+                        type="text"
+                        class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
+                        id="exampleFormControlInput50"
+                        value=""
+                      />
+                      <label
+                        for="exampleFormControlInput50"
+                        class="absolute left-3 top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
+                        >City <span class="text-red-500">*</span>
+                      </label>
+                    </div>
+                    <div class="relative mb-3 w-full">
+                      <input
+                        type="text"
+                        class="peer block min-h-[auto] w-full rounded-xl border-2 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary placeholder:opacity-100 motion-reduce:transition-none"
+                        id="exampleFormControlInput50"
+                        value=""
+                      />
+                      <label
+                        for="exampleFormControlInput50"
+                        class="absolute left-3 top-0 mb-0 z-10 text-xl px-2 origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-gray-900 transition-all duration-200 ease-out bg-white peer-focus:text-primary -translate-y-[0.9rem] scale-[0.8] motion-reduce:transition-none dark:peer-focus:text-primary"
+                        >State <span class="text-red-500">*</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <!-- <div class="flex items-center gap-5">
+                      <div class="relative mb-3 w-full">
+                        <div class="gap-5">
+                          <label for="">Preferred Date <span class="text-red-500">*</span> </label>
+                          <div class="pt-2">
+                            <input
+                              type="date"
+                              class="py-2 px-6 rounded-2xl w-full border-2 border-grau-400"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div class="relative mb-3 w-full">
+                        <div class="gap-5">
+                          <label for="">Preferred Time <span class="text-red-500">*</span> </label>
+                          <div class="pt-2">
+                            <input
+                              type="time"
+                              class="py-2 px-6 rounded-2xl w-full border-2 border-grau-400"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div> -->
+                <div>
+                  <p for="" class="mb-3">Product Type <span class="text-red-500">*</span></p>
+                  <Select>
+                    <SelectTrigger class="w-full">
+                      <SelectValue placeholder="Select a product" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Product</SelectLabel>
+                        <SelectItem value="Educate The Children"> Sounds </SelectItem>
+                        <SelectItem value="Shelter The Homeless"> Electronics </SelectItem>
+                        <SelectItem value="Comfort The Sick"> Food Items </SelectItem>
+                        <SelectItem value="Enhance Social Justice"> Furniture </SelectItem>
+                        <!-- <SelectItem value="Feed The Hungry"> Feed The Hungry </SelectItem> -->
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+
+                <button
+                  type="submit"
+                  class="w-[350px] h-[45px] rounded-2xl bg-teal-800 text-white font-bold mx-auto flex items-center text-center justify-center"
+                >
+                  Create
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </DefaultLayout>
+</template>
+<script setup>
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import SupplySidebar from '/src/views/Supply/SupplySidevar.vue'
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue
+} from '/components/ui/select'
+</script>
---
+++ src/views/Supply/SupplierList.vue
@@ -0,0 +1,122 @@
+<template>
+  <DefaultLayout>
+    <div class="bg-slate-50">
+      <div class="pt-20 flex justify-between">
+        <SupplySidebar />
+        <div class="px-[5%] bg-white py-5 w-3/4">
+          <div class="flex justify-between items-center pt-4">
+            <p class="text-2xl text-teal-600 font-bold pb-2">Supplier List</p>
+          </div>
+          <hr />
+          <div class="grid grid-cols-1 gap-5 my-5">
+            <div
+              class="rounded-md px-2 py-3 flex gap-5 justify-between bg-gray-100 shadow-md items-center"
+            >
+              <table class="table-auto w-full">
+                <thead>
+                  <tr class="bg-white text-xl">
+                    <th class="p-2 text-left">Supplier ID</th>
+                    <th class="p-2 text-left">Supplier Name</th>
+                    <th class="p-2 text-left">Address</th>
+                    <th class="p-2 text-left">Product Type</th>
+                    <th class="p-2 text-left">Status</th>
+                    <th class="p-2 text-left">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr class="">
+                    <td>
+                        <p>
+                            01
+                        </p>
+                    </td>
+                    <td class="py-4 p-2 flex items-center gap-2">
+                      <img src="/src/assets/image/home/l4.jpg" alt="" class="h-10" />
+                      <h3 class="font-bold">Dallas Sounds</h3>
+                    </td>
+                    <td class="py-4 p-2">1 Jan 2024</td>
+                    <td class="py-4 p-2 font-semibold">Sounds</td>
+                    <td class="py-4 p-2">
+                      <p class="text-sm font-bold text-green-600">Completed</p>
+                    </td>
+                    <td class="py-4 p-2">
+                      <button class="px-3 py-2 rounded-md shadow-md bg-teal-600 text-white text-sm">
+                        Details
+                      </button>
+                    </td>
+                  </tr>
+                  <tr class="">
+                    <td>
+                        <p>
+                            01
+                        </p>
+                    </td>
+                    <td class="py-4 p-2 flex items-center gap-2">
+                      <img src="/src/assets/image/home/l4.jpg" alt="" class="h-10" />
+                      <h3 class="font-bold">Dallas Sounds</h3>
+                    </td>
+                    <td class="py-4 p-2">1 Jan 2024</td>
+                    <td class="py-4 p-2 font-semibold">Sounds</td>
+                    <td class="py-4 p-2">
+                      <p class="text-sm font-bold text-green-600">Completed</p>
+                    </td>
+                    <td class="py-4 p-2">
+                      <button class="px-3 py-2 rounded-md shadow-md bg-teal-600 text-white text-sm">
+                        Details
+                      </button>
+                    </td>
+                  </tr>
+                  <tr class="">
+                    <td>
+                        <p>
+                            01
+                        </p>
+                    </td>
+                    <td class="py-4 p-2 flex items-center gap-2">
+                      <img src="/src/assets/image/home/l4.jpg" alt="" class="h-10" />
+                      <h3 class="font-bold">Dallas Sounds</h3>
+                    </td>
+                    <td class="py-4 p-2">1 Jan 2024</td>
+                    <td class="py-4 p-2 font-semibold">Sounds</td>
+                    <td class="py-4 p-2">
+                      <p class="text-sm font-bold text-green-600">Completed</p>
+                    </td>
+                    <td class="py-4 p-2">
+                      <button class="px-3 py-2 rounded-md shadow-md bg-teal-600 text-white text-sm">
+                        Details
+                      </button>
+                    </td>
+                  </tr>
+
+
+                </tbody>
+              </table>
+            </div>
+            <!-- <div
+                class="rounded-md px-2 py-3 flex gap-5 justify-between bg-gray-100 shadow-md items-center"
+              >
+                <div class="flex gap-5 items-center">
+                  <img src="/src/assets/image/home/l4.jpg" alt="" class="h-10" />
+                  <h3 class="font-bold">Technology and Media Outreach</h3>
+                  <p class="font-semibold text-teal-600">
+                    1 Jan 2024<span class="text-black font thin px-2">to</span>20 Jan 2024
+                  </p>
+                  <p class="font-semibold text-teal-600">
+                    Location : <span class="text-black">Masjid Premises</span>
+                  </p>
+                  <p class="text-sm font-bold text-green-600">Complete</p>
+                </div>
+                <button class="px-3 py-2 rounded-md shadow-md bg-teal-600 text-white text-sm">
+                  Details
+                </button>
+              </div> -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </DefaultLayout>
+</template>
+<script setup>
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import SupplySidebar from '/src/views/Supply/SupplySidevar.vue'
+</script>
