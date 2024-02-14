8462d62ec797b84b218881a9e2916c51bd8598e0
Jahirul Islam Razu <jirrazu123@gmail.com>
2024-02-15 04:31:33 +0600

    Hr sidebar edit

--- src/views/Hr/HrSidevar.vue
+++ src/views/Hr/HrSidevar.vue
@@ -21,9 +21,9 @@
         >Profile</router-link
       >
 
-            <!--Job Management  -->
+            <!--  -->
             <div class="accordion">
-        <button @click="toggleAccordion('vendorManagement')" class="flex items-center gap-2">
+        <button @click="drop = 1"  class="flex items-center gap-2">
           <span class="text-blue-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="m9 13.5 3 3m0 0 3-3m-3 3v-6m1.06-4.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
@@ -32,7 +32,7 @@
         
           Manage Organogarm
         </button>
-        <div v-show="currentAccordions === 'vendorManagement'" class="submenu mx-8">
+        <div v-show="drop == 1" class="submenu mx-8">
           <router-link to="/HR_Define_Position" class="flex items-center gap-2"
             >- Define Position </router-link
           >
@@ -46,7 +46,7 @@
 
       <!--Job Management  -->
       <div class="accordion">
-        <button @click="toggleAccordion('vendorManagement')" class="flex items-center gap-2">
+        <button @click="drop = 2" class="flex items-center gap-2">
           
           <span class="text-blue-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -57,7 +57,7 @@
 
           Job Management
         </button>
-        <div v-show="currentAccordions === 'vendorManagement'" class="submenu mx-8">
+        <div v-show="drop == 2" class="submenu mx-8">
           <router-link to="/HR_Create_Job_Description" class="flex items-center gap-2"
             >- Create  Job Description </router-link
           >
@@ -73,7 +73,7 @@
 
      <!--Higiring management   -->
      <div class="accordion">
-        <button @click="toggleAccordion('vendorManagement')" class="flex items-center gap-2">
+        <button @click="drop = 3" class="flex items-center gap-2">
           <span class="text-blue-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
@@ -83,7 +83,7 @@
           
           Higiring management
         </button>
-        <div v-show="currentAccordions === 'vendorManagement'" class="submenu mx-8">
+        <div v-show="drop == 3" class="submenu mx-8">
           <router-link to="/HR_Higiring_Application" class="flex items-center gap-2"
             >- Application steps</router-link
           >
@@ -100,7 +100,7 @@
     
     <!--Employee management   -->
     <div class="accordion">
-        <button @click="toggleAccordion('vendorManagement')" class="flex items-center gap-2">
+        <button @click="drop = 4" class="flex items-center gap-2">
           <span class="text-blue-500">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z" />
@@ -110,7 +110,7 @@
           
           Employee management
         </button>
-        <div v-show="currentAccordions === 'vendorManagement'" class="submenu mx-8">
+        <div v-show="drop == 4" class="submenu mx-8">
           <router-link to="/HR_Employee_list" class="flex items-center gap-2"
             >- Employee list</router-link
           >
@@ -168,17 +168,7 @@ const router = useRouter()
 const store = useAuthStore()
 
 //accordion
- 
-const currentAccordions = ref('bidManagement')
-
-const toggleAccordion = (accordionName) => {
-  if (accordionName === currentAccordions.value) {
-    currentAccordions.value = ''
-  } else {
-    currentAccordions.value = accordionName
-  }
-}
-
+const drop = ref(0)
 
 watch(
   () => store.isAuthenticated,
