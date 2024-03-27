82d42d81fb5f141df02d0d72e6ce65a10f86a8c7
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2024-03-27 21:38:07 +0600

    postimage page design

--- src/views/MediaAndCom/Gallery/PostImage.vue
+++ src/views/MediaAndCom/Gallery/PostImage.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="px-5 bg-slate-50 py-5 w-3/4">
-          <p class="text-2xl font-bold py-3 border-b">Create Event </p>
+  <div class="px-5  py-5 w-3/4">
+          <p class="text-2xl font-bold py-3 border-b">Post Image </p>
+          <hr />
           <div class="bg-white rounded-xl p-5 w-full shadow-md mt-5">
             <form @submit.prevent="socialEventSubmit()">
               <div class="py-5 space-y-7 mt-5">
