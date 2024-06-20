3f878f8f606a5a597a310afd2f71089ec5cd0f28
Arif Akib <a.rabbani.akib@gmail.com>
2024-06-20 23:57:37 +0600

    minor change

--- src/components/HomeFolder/announcement.vue
+++ src/components/HomeFolder/announcement.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full bg-[url('../assets/image/home/pattern-1.png')] bg-repeat">
+  <div class="w-full bg-repeat">
     <div class="w-full px-5">
       <h1 class="text-3xl font-bold uppercase pb-3">Announcement</h1>
       <div class="flex gap-36">
