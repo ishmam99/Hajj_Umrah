d9b6bae8c4218deb62fb773e1f191a791538d795
Jahidul Islam <56561463+sammijahidul@users.noreply.github.com>
2024-06-26 02:51:23 +0600

    student apply, teacher apply page done

--- src/router/index.ts
+++ src/router/index.ts
@@ -1143,9 +1143,7 @@ const router = createRouter({
           component: () => import('../views/Member/MemberAppointmentScholar.vue')
         },
       ],
-    },
-    
-   
+    },  
 
     {
       path: '/learning-center',
@@ -1196,6 +1194,19 @@ const router = createRouter({
       component: () => import('../auth/RegistrationPage.vue'),
       meta: { layout: AuthLayout, notAuthenticated: true }
     },
+    {
+      path: '/student/registration',
+      name: 'RegistrationPageForStudent',
+      component: () => import('../views/website/eLearning/Student/StudentApply.vue'),
+      meta: { layout: AuthLayout, notAuthenticated: true }
+    },
+    {
+      path: '/teacher/registration',
+      name: 'RegistrationPageForTeacher',
+      component: () => import('../views/website/eLearning/Teacher/TeacherApply.vue'),
+      meta: { layout: AuthLayout, notAuthenticated: true }
+    },
+
     {
       path: '/reg-teacher',
       name: 'TeacherRegistrationPage',
