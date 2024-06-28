a8014fded4cfac1e1aaabfd8579bc5587fe41290
Rasik Abdullah <rasikabdullah@gmail.com>
2024-06-28 21:13:21 +0600

    update dashboard

--- src/views/LearningManagementSystem/CourseManagement/CourseList.vue
+++ src/views/LearningManagementSystem/CourseManagement/CourseList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="px-4 bg-white py-5 w-3/4">
+  <div class="px-4 bg-white py-5">
     <div v-if="!selectedCourse">
       <div class="flex justify-between items-center pt-4">
         <p class="text-2xl text-cyan-700 font-bold pb-2">Course List</p>
