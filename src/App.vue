de31bccfb360c45af7903806b369c9d5aa0659c1
sadikBB <sadik@bookblaster.io>
2024-09-04 22:52:04 +0600

    test commit
    test commit

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <component :is="$route.meta.layout || 'div'">
     <router-view></router-view>
   </component>
-  <!-- <router-view class=""></router-view> -->
+  <!-- <router-view class=""></router-view> Awwwww -->
 </template>
 
 <script setup></script>
